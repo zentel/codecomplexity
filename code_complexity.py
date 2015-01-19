1e53cfb7228f086758e0822f763de88316529b15
Santiago <santiago.blanco@gmail.com>
2015-01-19 13:05:40 -0300

    Versión 1.0
    Versión 1.0 funcionando, generando el reporte final de PL_SQL

--- code_complexity.py
+++ code_complexity.py
@@ -1,5 +1,6 @@
 ﻿import os
 import sys
+import math
 
 def cyclomatic_complexity(edges, nodes, components):
     """Dada la cantidad de caminos posibles en el codigo (edges), la cantidad de nodos(nodes) y la cantidad de componentes - o binarios intervinientes - (components) calcula la complejidad ciclomatica, lo que demuestra cuan complejo es el flujo del codigo"""
@@ -8,38 +9,68 @@ def cyclomatic_complexity(edges, nodes, components):
 def halstead_complexity(operators, operands, total_operators, total_operands):
     """ Dada la cantidad de operadores y operandos unicos (sin repeticiones), y la cantidad total de operadores y operandos (con repeticiones) se realizan los diferentes calculos de Halstead para medir complejidad:
 		Vocabulario: vocabulary = operators + operands
-		Longitud: length = total_operators + total_operands
+		length: length = total_operators + total_operands
 		Longitud calculada: calculated_length = operators*log(operators) + operands*log(operands)
 		Volumen (describe el tamano de la implementacion de un algoritmo. Debe estar entre 20 y 1000 para una funcion/metodo): volume = length*log(vocabulary)
-		Dificultad (nivel de dificultad para mantener, lo que lleva a mayor propension al error): difficulty = (operators/2)*(total_operands/operands)
-		Esfuerzo (mide el esfuerzo de entender e implementar el codigo): effort = volume*difficulty """
-    pass
-	
+		Dificultad (nivel de dificultad para mantener, lo que lleva a mayor propension al error): difficulty = (operators/2)*(total_operands/operands) """
+    halstead = Halstead()
+    try:        
+        halstead.vocabulary = operators + operands
+        halstead.length = total_operators + total_operands
+        
+        if operators != 0 and operands != 0:
+            halstead.calculated_length = operators*math.log(operators)+operands*math.log(operands)
+        else:
+            halstead.calculated_length = 0.0
+            
+        if halstead.vocabulary > 0:
+            halstead.volume = halstead.length*math.log(halstead.vocabulary)
+        else: 
+            halstead.volume = 0.0
+            
+        if operands != 0:
+            halstead.difficulty = (operators/2)*(total_operands/operands)
+        else:
+            halstead.difficulty = 0.0
+           
+    except ValueError:
+        print("Error°. Valores: {0:4d}, {1:4d}, {2:4d}, {3:4d} ", operators, operands, total_operators, total_operands)
+    return halstead
+    
 def calcular_complejidad(function_count, output, path ="."):
     """ realiza el reporte de complejidad para todos los archivos en un path determinado, aplicando la funcion establecida para contabilizar, y escribiendo el reporte en output"""
-    
-    os.chdir(path)
-    path_corregido = os.getcwd()
-    coleccion_contadores = []
-    archivos = os.listdir()
-    
-    for archivo in archivos:
-        if os.path.isdir(path_corregido + "\\" + archivo): #es un directorio, recorrerlo
-            print("Directorio: " + path_corregido + "\\" + archivo)
-            os.chdir(path_corregido + "\\" + archivo)
-            coleccion_contadores.extend(calcular_complejidad_int(function_count, path_corregido + "\\" + archivo))
-        elif os.path.isfile(path_corregido + "\\" + archivo): #es un archivo, procesarlo
-            coleccion_contadores.append(function_count(path_corregido + "\\" + archivo))
-        else:
-            print("Ouch! no es un archivo")
-            
-    for c in coleccion_contadores:
-        print("{0:60}|{1:3d}|{2:3d}|{3:3d}|{4:3d}|{5:3d}|{6:3d}|{7:3d}|{8:3d}".format(c.archivo,c.lines, c.edges, c.nodes, c.components, c.operators, c.operands, c.total_operators, c.total_operands))
-    
-    print ("{0:4d} archivos procesados".format(len(archivos)))   # TODO: ARREGLAR!!!!
+    with open(".\\log.txt","w") as log:
+        os.chdir(path)
+        path_corregido = os.getcwd()
+        coleccion_contadores = []
+        archivos = os.listdir()
+        
+        for archivo in archivos:
+            if os.path.isdir(path_corregido + "\\" + archivo): #es un directorio, recorrerlo
+                log.write("[DEBUG] Directorio: " + path_corregido + "\\" + archivo + "\n")
+                os.chdir(path_corregido + "\\" + archivo)
+                coleccion_contadores.extend(calcular_complejidad_int(function_count, path_corregido + "\\" + archivo, log))
+            elif os.path.isfile(path_corregido + "\\" + archivo): #es un archivo, procesarlo
+                coleccion_contadores.append(function_count(path_corregido + "\\" + archivo, log))
+            else:
+                log.write("[WARNING] " + path_corregido + "\\" + archivo + " no es un archivo\n")
+                
+        cant_archivos = 0
+        
+        halstead_obj = Halstead()
+        
+        with open(output,"w") as file:
+            file.write("{0:60}|{1:25}|{2:25}|{3:25}|{4:25}|{5:25}|{6:25}|{7:25}|{8:25}|{9:25}|{10:25}|{11:25}|{12:25}|{13:25}|{14:25}\n".format("ARCHIVO","LINES", "EDGES", "NODES", "COMPONENTS", "OPERATORS", "OPERANDS", "TOT.OPERATORS", "TOT.OPERANDS", "CYCLOMATIC COMPLEX","HALSTEAD.VOCABULARY","HALSTEAD.LENGTH", "HALSTEAD.CALC.LENGTH", "HALSTEAD.VOLUME", "HALSTEAD.DIFFICULTY"))
+            for c in coleccion_contadores:
+                halstead_obj = halstead_complexity(c.operators, c.operands, c.total_operators, c.total_operands)
+                cant_archivos+=1
+                file.write("{0:60}|{1:25d}|{2:25d}|{3:25d}|{4:25d}|{5:25d}|{6:25d}|{7:25d}|{8:25d}|{9:25d}|{10:25d}|{11:25d}|{12:25}|{13:25}|{14:25}\n".format(c.archivo,c.lines, c.edges, c.nodes, c.components, c.operators, c.operands, c.total_operators, c.total_operands, cyclomatic_complexity(c.edges, c.nodes, c.components), halstead_obj.vocabulary, halstead_obj.length, halstead_obj.calculated_length, halstead_obj.volume, halstead_obj.difficulty))
+        log.write("[INFO] " + repr(cant_archivos)+" archivos procesados.\n")
+        
+    print (repr(cant_archivos)+" archivos procesados.")
         
 
-def calcular_complejidad_int(function_count, path):
+def calcular_complejidad_int(function_count, path, log_file):
     """ Función interna, para aplicar recursividad entre los directorios donde se encuentren los fuentes"""
     os.chdir(path)
     path_corregido = os.getcwd()
@@ -48,13 +79,13 @@ def calcular_complejidad_int(function_count, path):
     
     for archivo in archivos:
         if os.path.isdir(path_corregido + "\\" + archivo): #es un directorio, entonces hay que recorrerlo
-            print("Directorio: " + path_corregido + "\\" + archivo)
+            log_file.write("[INFO] Directorio: " + path_corregido + "\\" + archivo + "\n")
             os.chdir(path_corregido + "\\" + archivo)            
-            coleccion_contadores.extend(calcular_complejidad_int(function_count, path_corregido + "\\" + archivo))
+            coleccion_contadores.extend(calcular_complejidad_int(function_count, path_corregido + "\\" + archivo, log_file))
         elif os.path.isfile(path_corregido + "\\" + archivo):  #es un archivo, procesarlo
-            coleccion_contadores.append(function_count(path_corregido + "\\" + archivo))
+            coleccion_contadores.append(function_count(path_corregido + "\\" + archivo, log_file))
         else:
-            print("Ouch! " + repr(sys.exc_info()[0]))
+            log_file.write("[ERROR] " + repr(sys.exc_info()[0]))
     
     return coleccion_contadores
     
@@ -70,6 +101,14 @@ class Contadores:
     operands = 0
     total_operators = 0
     total_operands = 0
+
+class Halstead:
+    """ Clase que es utilizada para gestionar la información resultante de los cálculos de complejidad de Halstead"""
+    vocabulary = 0
+    length = 0
+    calculated_length = 0.0
+    volume = 0.0
+    difficulty = 0.0
     
 if __name__ == "__main__":
     import sys
--- code_complexity_PL_SQL.py
+++ code_complexity_PL_SQL.py
@@ -2,7 +2,7 @@
 
 import code_complexity
 
-def count_PL(file_name):
+def count_PL(file_name, log_file):
     """ Cuenta los elementos en un archivo PL para poder realizar los calculos de complejidad """
     # operadores de PL_SQL para contar dentro del código
     PL_SQL_operators = "+ - * / ** = != <> ~= < > >= <= LIKE BETWEEN IN ISNULL AND OR NOT DECLARE BEGIN END IF THEN ELSE CASE WHEN WHILE FOR CREATE REPLACE TYPE PROCEDURE EXEC DROP SELECT UPDATE DELETE FUNCTION RETURN AS END FROM WHERE ORDER BY RECORD EXCEPTION TRIGGER PACKAGE BODY LOOP ROLLBACK SAVEPOINT COMMIT SCALAR LOB NUMERIC CHARACTER BOOLEAN DATETIME PLS_INTEGER BINARY_INTEGER BINARY_FLOAT BINARY_DOUBLE NUMBER DEC DECIMAL DOUBLE FLOAT INT INTEGER SMALLINT REAL CHAR VARCHAR2 RAW NCHAR NVARCHAR2 LONG RAW ROWID UROWID TABLE YEAR MONTH DAY HOUR MINUTE SECOND TIMEZONE BFILE BLOB CLOB NCLOB CONSTANT GROUP ("
@@ -13,12 +13,13 @@ def count_PL(file_name):
     cont.lines = 0
     linea = ""
     elementos = {}
+    PL_operators_list = PL_SQL_operators.split(" ")
     
     with open(file_name) as arq:
         try:
             linea = arq.readline()
         except UnicodeDecodeError:
-            print("Error de codificación Unicode en archivo " + file_name)
+            log_file.write("[ERROR] Error de codificación Unicode en archivo " + file_name + "\n")
         
         while(linea != ""):
             try:
@@ -28,23 +29,40 @@ def count_PL(file_name):
                 linea = linea.replace(",", "")
                 linea = linea.replace(";", "")
                 linea = linea.replace(":", "")
+                #linea = linea.replace("\n", "")
                 
                 splitted = linea.split(" ")
                 for o in splitted:
-                    if o in elementos:
-                        elementos[o] += 1
+                    cleaned_o = o.replace("\n", "")
+                    cleaned_o = cleaned_o.replace("\t","")
+                    if cleaned_o in elementos:
+                        elementos[cleaned_o] += 1
                     else:
-                        elementos[o] = 1
+                        elementos[cleaned_o] = 1
                         
             except UnicodeDecodeError:
-                print("Error de codificación Unicode en archivo " + file_name)
+                log_file.write("[ERROR] Error de codificación Unicode en archivo " + file_name + "\n")
+                
         for k in elementos.keys():
-            if PL_SQL_operators.find(k):
+            cleaned_k = k.replace("\n", "")
+            cleaned_k = cleaned_k.replace("\t","")
+                        
+            if cleaned_k in PL_operators_list:
+                try:
+                    log_file.write("[DEBUG] key encontrada en el archivo {0} es {1} y representa {2}\n".format(file_name, repr(cleaned_k),"operador"))
+                except:
+                    log_file.write("[ERROR] generando log")
+                    
                 cont.operators +=1
-                cont.total_operators += elementos[k]
+                cont.total_operators += elementos[cleaned_k]
             else:
+                try:
+                    log_file.write("[DEBUG] key encontrada en el archivo {0} es {1} y representa {2}\n".format(file_name, repr(cleaned_k),"operando"))
+                except:
+                    log_file.write("[ERROR] generando log")
+                    
                 cont.operands +=1
-                cont.total_operands += elementos[k]
+                cont.total_operands += elementos[cleaned_k]
         
         #Valores para la complejidad ciclomática
         #Por defecto, hay un path y un nodo para recorrer
@@ -62,6 +80,10 @@ def count_PL(file_name):
         if "CASE" in elementos.keys(): #el case agrega un nodo y un paths
             cont.edges += elementos["CASE"]
             cont.nodes += elementos["CASE"] 
+            
+        if "EXCEPTION" in elementos.keys(): #el exception agrega un nodo y dos paths
+            cont.edges += elementos["EXCEPTION"] * 2
+            cont.nodes += elementos["EXCEPTION"]
             
         if "EXEC" in elementos.keys(): #el exec agrega un nodo y un paths
             cont.edges += elementos["EXEC"]
@@ -89,6 +111,21 @@ def count_PL(file_name):
             cont.components += elementos["BEGIN"]
         else:
             cont.components = 1
+            
+        if "PROCEDURE" in elementos.keys(): # el begin agrega un nodo y un paths (ademas de un elemento)
+            cont.edges += elementos["PROCEDURE"]
+            cont.nodes += elementos["PROCEDURE"]
+            cont.components += elementos["PROCEDURE"]
+        else:
+            cont.components = 1
+            
+        if "FUNCTION" in elementos.keys(): # el begin agrega un nodo y un paths (ademas de un elemento)
+            cont.edges += elementos["FUNCTION"]
+            cont.nodes += elementos["FUNCTION"]
+            cont.components += elementos["FUNCTION"]
+        else:
+            cont.components = 1    
+        
             
         if "WHERE" in elementos.keys(): 
             cont.edges += elementos["WHERE"] + cont.operands #en el caso de selects, la complejidad viene dada por la cantidad de variables que se manejan
